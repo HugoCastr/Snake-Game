b1500740fd868731f63bad7994c67b43ffceb416
Hugo Castro <153029576+HugoCastr@users.noreply.github.com>
2024-11-05 15:07:26 -0300

    Add files via upload
    Atualizacao do codigo, esta incompleto ainda

--- SnakeGame/Tabuleiro.java
+++ SnakeGame/Tabuleiro.java
@@ -11,6 +11,7 @@ public class Tabuleiro extends JFrame {
     private JButton iniciarButton;
     private JButton resetButton;
     private JButton pauseButton;
+    private JButton modeButton;
     private JTextField placarField;
     private int x, y;
     private String direcao = "direita";
@@ -20,14 +21,24 @@ public class Tabuleiro extends JFrame {
     private int larguraTabuleiro, alturaTabuleiro;
     private int placar = 0;
 
+    private ArrayList<Quadrado> corpo = new ArrayList<>();
+    private boolean pausar = true;
+    private int modo = 1;
+    private String dificuldade = "Facil";
+
     public Tabuleiro() {
 
         larguraTabuleiro = alturaTabuleiro = 400;
 
         // ArrayList<Quadrado> corpo = new ArrayList<Quadrado>();
         // Quadrado novoQuadrado = new Quadrado(10, 10, Color.BLACK);
+        corpo = new ArrayList<>();
+        // Quadrado cobra = new Quadrado(10, 10, Color.BLACK);
 
         cobra = new Quadrado(10, 10, Color.BLACK);
+
+        corpo.add(cobra);
+
         cobra.x = larguraTabuleiro / 2;
         cobra.y = alturaTabuleiro / 2;
 
@@ -44,12 +55,14 @@ public class Tabuleiro extends JFrame {
         iniciarButton = new JButton("Iniciar");
         resetButton = new JButton("Reiniciar");
         pauseButton = new JButton("Pausar");
+        modeButton = new JButton(dificuldade);
         placarField = new JTextField("Placar: 0");
         placarField.setEditable(false);
 
         menu.add(iniciarButton);
         menu.add(resetButton);
         menu.add(pauseButton);
+        menu.add(modeButton);
         menu.add(placarField);
 
         painel = new JPanel() {
@@ -85,6 +98,11 @@ public class Tabuleiro extends JFrame {
         pauseButton.addActionListener(e -> {
             Pausar();
 
+        });
+
+        // ActionListener para o botao trocar a dificuldade
+        modeButton.addActionListener(e -> {
+            DificuldadeJogo();
         });
 
         painel.addKeyListener(new KeyAdapter() {
@@ -127,7 +145,7 @@ public class Tabuleiro extends JFrame {
 
     private void Maca() { // completo
         int min = 20; // define o minimo da largura e altura do tabuleiro
-        int max = alturaTabuleiro - 20; // define o maximo da largura e altura do tabuleiro
+        int max = alturaTabuleiro - min; // define o maximo da largura e altura do tabuleiro
         obstaculo = new Quadrado(10, 10, Color.red); // cria um novo obstaculo
         obstaculo.x = (int) (Math.random() * (max - min + 1)) - min; // randomiza a posicao x
         obstaculo.y = (int) (Math.random() * (max - min + 1)) - min; // randomiza a posicao y
@@ -140,32 +158,50 @@ public class Tabuleiro extends JFrame {
                                                                             // ou abaixo da maca
 
             placar++; // incrementa o placar
-            Maca(); // chama a funcao para e cria uma nova maca no tabuleiro
             Aumentar(); // chama a funcao para aumentar o tamanho da cobra
+            Maca(); // chama a funcao para e cria uma nova maca no tabuleiro
         }
     }
 
     private void Aumentar() { // em andamento
-        // ArrayList<Quadrado> corpo = new ArrayList<Quadrado>();
-        // Quadrado novoQuadrado = new Quadrado(10, 10, Color.BLACK);
 
-        // corpo.add(novoQuadrado);
-        // arrumar uma maneira no qual adicone mais um quadro 
-        // e como vai funcionar para ele seguir
+        Quadrado cabeca = corpo.get(0); // pega a posicao da cabeca
+        Quadrado novoQuadrado = new Quadrado(cabeca.x, cabeca.y, Color.BLACK); // cria um novo quadrado na posicao da
+                                                                               // cabeca
+
+        corpo.add(novoQuadrado); // adicona o novo quadrado ao corpo
 
     }
 
-    private void Iniciar() {
+    private void MovimentoDaCobra() { // em andamento
+        for (int i = corpo.size() - 1; i > 0; i--) {
+            
+            corpo.get(i).x = corpo.get(i - 1).x;
+            corpo.get(i).y = corpo.get(i - 1).y;
+        }
+    }
 
+    private void ColisaoNasBordas() {
+        if ((cobra.x < 0 || cobra.x > alturaTabuleiro) // verifica se a cobra esta no eixo x igual a 0 ou o equivalente
+                                                       // ao tamanho do tabuleiro
+                || // ou
+                (cobra.y < 0 || cobra.y > alturaTabuleiro)) { // verifica se a cobra esta no eixo y igual a 0 ou o
+                                                              // equivalente ao tamanho do tabuleiro
+            GameOver(); // chama a funcao para informar a perca
+        }
+    }
+
+    private void Iniciar() {
+        pausar = true;
         new Thread(() -> {
-            while (true) {
+            while (pausar) {
                 try {
                     Thread.sleep(tempoAtualizacao);
 
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
+                MovimentoDaCobra();
                 switch (direcao) {
                     case "esquerda":
                         cobra.x -= incremento;
@@ -181,6 +217,7 @@ public class Tabuleiro extends JFrame {
                         break;
 
                 }
+
                 ColisaoComMaca();
                 painel.repaint();
 
@@ -190,22 +227,60 @@ public class Tabuleiro extends JFrame {
 
     private void Reiniciar() {
         // Adicione aqui a lógica para reiniciar o jogo
+        for (int i = corpo.size() - 1; i > 0; i--) {
+            corpo.remove(i);
+        }
+        pausar = (!pausar);
         cobra.x = larguraTabuleiro / 2; // faz com que a cobra retorne ao meio do tabuleiro
         cobra.y = alturaTabuleiro / 2;
         Maca(); // cria uma nova maca
         placar = 0; // reseta o placar
-        // terminar isso
-        // fazer com que a cobra pare no meio apos reiniciar
+        Iniciar();
+
         JOptionPane.showMessageDialog(this, "Jogo Reiniciado!", "Reset", JOptionPane.INFORMATION_MESSAGE);
     }
 
     private void Pausar() {
         // Interrompe o while(!reset) do método Iniciar() pausando o jogo.
+        pausar = (!pausar);
 
         JOptionPane.showMessageDialog(this, "Jogo Pausado!", "Pause", JOptionPane.INFORMATION_MESSAGE);
     }
 
+    private void GameOver() {
+        JOptionPane.showMessageDialog(this, "Gamer Over", "A", JOptionPane.INFORMATION_MESSAGE);
+        Reiniciar();
+    }
+
+    private void DificuldadeJogo() { // em andamento
+        modo++; // incrementa o valor
+
+        if (modo > 3) { // caso o valor seja maior que 3
+            modo = 1; // define o valor para 1 novamente
+        }
+        switch (modo) {
+            case 1 -> {
+                dificuldade = "Facil";
+
+                ColisaoComMaca(); // chama a funcao para verificar a colisao com a maca
+            }
+            case 2 -> {
+                dificuldade = "Medio";
+                ColisaoComMaca(); // chama a funcao para verificar a colisao com a maca
+                ColisaoNasBordas(); // chama a funcao para verificar a colisao com a borda
+            }
+            case 3 -> {
+                dificuldade = "Dificil";
+                ColisaoComMaca(); // chama a funcao para verificar a colisao com a maca
+                ColisaoNasBordas(); // chama a funcao para verificar a colisao com a borda
+            }
+        }
+
+        modeButton.setText(dificuldade); // Atualiza o texto do botão com a nova dificuldade
+    }
+
     public static void main(String[] args) {
         new Tabuleiro();
+
     }
 }
